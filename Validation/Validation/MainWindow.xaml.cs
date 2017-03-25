8e1616cb2fe837e164bf986851b9bd11fa6fc0f1
igortereshchenko <tereshchenko.igor@gmail.com>
2017-03-25 05:55:39 -0700

    added validation

---
+++ Validation/Validation/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Validation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// https://blog.magnusmontin.net/2013/08/26/data-validation-in-wpf/
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel();
+            
+        }
+    }
+}
---
+++ Validation/Validation/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            /* Set default age */
+            this.Age = 30;
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 10 || value > 100)
+                    throw new ArgumentException("The age must be between 10 and 100");
+                _age = value;
+            }
+        }
+
+    }
+}
